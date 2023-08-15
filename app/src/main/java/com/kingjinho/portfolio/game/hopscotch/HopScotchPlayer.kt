a2379524ab0aa981dccd7769dc3729882b79166c
kingjinho <jinhoshin319@gmail.com>
2023-08-15 11:12:11 +0900

    refactor: rename sealed class to hopscotch

--- app/src/main/java/com/kingjinho/portfolio/game/hopscotch/HopScotchPlayer.kt
+++ app/src/main/java/com/kingjinho/portfolio/game/hopscotch/HopScotchPlayer.kt
@@ -2,12 +2,12 @@ package com.kingjinho.portfolio.game.hopscotch
 
 import androidx.compose.ui.graphics.Color
 
-sealed class TouchTouchPlayer {
+sealed class HopScotchPlayer {
     val color: Color
         get() = when (this) {
             is Red -> Color.Red
             is Blue -> Color.Blue
         }
-    object Red : TouchTouchPlayer()
-    object Blue : TouchTouchPlayer()
+    object Red : HopScotchPlayer()
+    object Blue : HopScotchPlayer()
 }
