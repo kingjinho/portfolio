e6a5693b97c13668423fcc4401f14317eca9a18c
kingjinho <jinhoshin319@gmail.com>
2023-08-16 17:34:00 +0900

    feat: wrap composable with NavHost composable

--- app/src/main/java/com/kingjinho/portfolio/PortfolioApp.kt
+++ app/src/main/java/com/kingjinho/portfolio/PortfolioApp.kt
@@ -1,9 +1,92 @@
 package com.kingjinho.portfolio
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import com.kingjinho.portfolio.drawer.ScreenHomeRental
+import com.kingjinho.portfolio.game.hopscotch.HopScotchGameScreen
+import com.kingjinho.portfolio.game.touchtouch.TouchTouchGameScreen
 
 @Composable
 fun PortfolioApp() {
-    ScreenHomeRental()
+    val navController = rememberNavController()
+    NavHost(navController = navController, startDestination = Screen.Home.route) {
+        composable("home") {
+            HomeScreen(
+                onButtonClick = {
+                    navController.navigate(it.route)
+                }
+            )
+        }
+
+        composable(Screen.CustomDrawer.route) {
+            ScreenHomeRental()
+        }
+
+        composable(Screen.HopScotchGame.route) {
+            HopScotchGameScreen()
+        }
+
+        composable(Screen.TouchTouchGame.route) {
+            TouchTouchGameScreen()
+        }
+
+    }
+}
+
+private val screens = listOf(
+    Screen.CustomDrawer,
+    Screen.HopScotchGame,
+    Screen.TouchTouchGame
+)
+
+@Composable
+fun HomeScreen(onButtonClick: (Screen) -> Unit = {}) {
+    val scrollState = rememberScrollState()
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(horizontal = 24.dp)
+            .verticalScroll(scrollState),
+        verticalArrangement = Arrangement.Center
+    ) {
+
+        screens.forEach {
+            NavigationButton(
+                modifier = Modifier.padding(bottom = 16.dp),
+                item = it,
+                onButtonClick = onButtonClick
+            )
+        }
+    }
+}
+
+@Composable
+fun NavigationButton(
+    modifier: Modifier = Modifier,
+    onButtonClick: (Screen) -> Unit = {},
+    item: Screen
+) {
+    Button(
+        onClick = { onButtonClick(item) },
+        modifier = modifier.fillMaxWidth(),
+        shape = MaterialTheme.shapes.medium
+    ) {
+        Text(text = stringResource(item.titleRes))
+    }
+
 }
